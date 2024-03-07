086458c5ea60cbdca6d7087ea35cd02fb9e77b90
Ali <fizasohail512@gmail.com>
2024-03-07 22:04:31 +0500

    First commit

---
+++ index.js
@@ -0,0 +1,21 @@
+"use strict";
+// percentage program using if,else if and else
+let percentage = "57.2";
+if (percentage >= "90") {
+    console.log("A-one Grade");
+}
+else if (percentage >= "80") {
+    console.log("A Grade");
+}
+else if (percentage >= "70") {
+    console.log("B Grade");
+}
+else if (percentage >= "60") {
+    console.log("C Grade");
+}
+else if (percentage >= "50") {
+    console.log("D Grade");
+}
+else {
+    console.log("Fail");
+}
